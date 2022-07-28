cceb665de44d533244beabcc8ca11b0cd37c7e7a
Martin Braun <braun@neuroforge.de>
2022-07-28 21:14:57 +0200

    update bookstack to 22.07 (but tag as 22.07.0)

--- contrib/build.sh
+++ contrib/build.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
  docker build -f ../Dockerfile \
     -t neuroforgede/bookstack:latest \
-    -t neuroforgede/bookstack:22.06.2 \
+    -t neuroforgede/bookstack:22.07.0 \
     ../
 
 docker push neuroforgede/bookstack:latest
-docker push neuroforgede/bookstack:22.06.2
+docker push neuroforgede/bookstack:22.07.0
