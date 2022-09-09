f16fc4ea72e8399060532e231fc2175eaf64327d
Martin Braun <braun@neuroforge.de>
2022-09-09 14:04:14 +0200

    update to 22.09

--- contrib/build.sh
+++ contrib/build.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
  docker build -f ../Dockerfile \
     -t neuroforgede/bookstack:latest \
-    -t neuroforgede/bookstack:22.07.3 \
+    -t neuroforgede/bookstack:22.09.0 \
     ../
 
 docker push neuroforgede/bookstack:latest
-docker push neuroforgede/bookstack:22.07.3
+docker push neuroforgede/bookstack:22.09.0
