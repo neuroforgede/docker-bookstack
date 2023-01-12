b15cd9ea7dc64911163e93c3de5a9dbb354d922e
Martin Braun <braun@neuroforge.de>
2023-01-12 22:55:53 +0100

    bump to 22.11.1

--- contrib/build.sh
+++ contrib/build.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
  docker build -f ../Dockerfile \
     -t neuroforgede/bookstack:latest \
-    -t neuroforgede/bookstack:22.10.1 \
+    -t neuroforgede/bookstack:22.11.1 \
     ../
 
 docker push neuroforgede/bookstack:latest
-docker push neuroforgede/bookstack:22.10.1
+docker push neuroforgede/bookstack:22.11.1
