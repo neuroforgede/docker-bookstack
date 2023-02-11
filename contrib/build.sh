bc588cacd621dc6e5662d791e04b6ba08d8795f0
martinb <martinbraun123@aol.com>
2023-02-11 15:17:55 +0100

    remove contrib/build

--- contrib/build.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
- docker build -f ../Dockerfile \
-    -t neuroforgede/bookstack:latest \
-    -t neuroforgede/bookstack:22.11.1 \
-    ../
-
-docker push neuroforgede/bookstack:latest
-docker push neuroforgede/bookstack:22.11.1
